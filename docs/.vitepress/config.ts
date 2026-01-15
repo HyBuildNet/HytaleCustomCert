c4fa44c8fb609bc9494aa68b0876e8372e169fdb
Theo Andrup <theo.andrup@t-online.de>
2026-01-15 19:26:12 +0100

    docs

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -7,6 +7,7 @@ export default defineConfig({
   base: '/HytaleCustomCert/',
 
   head: [
+    ['meta', { name: 'google-site-verification', content: '-T2K0pWwX_CIpTMvP-RrEmr0nCenG4Nhw1YIl5NcjDQ' }],
     ['meta', { name: 'robots', content: 'index, follow' }],
     ['meta', { name: 'keywords', content: 'Hytale, plugin, TLS, certificate, reverse proxy, QUIC' }],
     ['link', { rel: 'canonical', href: 'https://hybuildnet.github.io/HytaleCustomCert/' }]
